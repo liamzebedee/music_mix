f92511b8b9a0c4c43a3e20a127ae74a22738865c
Liam Edwards-Playne <liamzebedee@yahoo.com.au>
2015-04-18 18:02:07 +1000

    1

--- findNearMe.js
+++ findNearMe.js
@@ -5,19 +5,37 @@ module.exports.process = function(request, response) {
 		location: {
 			lat: 121,
 			lng: 121
-		}
+		},
+		user_id: 123213213213
 
 	}
 	*/
 
-	// update user location
-	var usersWeWouldLike = [];
+	// Find user, update their location
+	var user_query = Parse.Query("User");
+	user_query.equalTo('facebook_id', data.user_id);
+	var user = null;
+	query.find({
+	  success: function(found) {
+	  	user = found;
+	  }
+	});
+	var user_location = new Parse.GeoPoint(data.location);
+	user.set('location', user_location);
+
+	// Find nearby users
+	var users_query = new Parse.Query("User");
+	var nearby_users = [];
+	var DISTANCE = 20; // km
+	users_query.withinKilometers("location", user_location, DISTANCE);
+	users_query.limit(1000);
+	users_query.find({
+	  success: function(nearby_users) {
+	  }
+	});
 
 	
 
-	// users_wed_like = []
-
-	// Locations.where(DISTANCE EQUATION < 5km):
 	// foreach user:
 	// 	include = false
 	// 	shared_interests = []
