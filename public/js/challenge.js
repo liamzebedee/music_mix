376cacfd0ee056d447c37a57065d2a0afa1e592b
Liam Edwards-Playne <liamzebedee@yahoo.com.au>
2015-04-19 07:08:34 +1000

    fuck it

--- public/js/challenge.js
+++ public/js/challenge.js
@@ -1,42 +1,53 @@
-(function(){
+$( window ).load(function() {
+var arc = {
+    fill: '#333',
+    stroke: '#333',
+    path: 'M53.286,44.333L69.081,7.904C48.084-1.199,23.615-2.294,0.648,6.78l14.59,36.928C28.008,38.662,41.612,39.27,53.286,44.333z'
+};
 
-	var button = document.getElementById('cn-button'),
-    wrapper = document.getElementById('cn-wrapper'),
-    overlay = document.getElementById('cn-overlay');
+var paper = Raphael(document.getElementById("notepad"), 800, 800);
 
-	//open and close menu when the button is clicked
-	var open = false;
-	button.addEventListener('click', handler, false);
-	wrapper.addEventListener('click', cnhandle, false);
+var arcDegrees = 45;
+var centerX = 210;
+var centerY = 210;
+var compassRadius = 68;
+var currentlyActive = 45;
+var directions = [
+    {label:'N', degrees:0, rotatedDegrees:270}, 
+    {label:'NE', degrees:45, rotatedDegrees:315}, 
+    {label:'E', degrees:90, rotatedDegrees:0}, 
+    {label:'SE', degrees:135, rotatedDegrees:45}, 
+    {label:'S', degrees:180, rotatedDegrees:90}, 
+    {label:'SW', degrees:225, rotatedDegrees:135}, 
+    {label:'W', degrees:270, rotatedDegrees:180}, 
+    {label:'NW', degrees:315, rotatedDegrees:225}
+];
 
-	function cnhandle(e){
-		e.stopPropagation();
-	}
+function arcClicked()
+{
+    var label = $(this).data('direction-label');
+    $("#activeArc").attr('id', null);
+    $(this).attr('id', 'activeArc');
+}
 
-	function handler(e){
-		if (!e) var e = window.event;
-	 	e.stopPropagation();//so that it doesn't trigger click event on document
-
-	  	if(!open){
-	    	openNav();
-	  	}
-	 	else{
-	    	closeNav();
-	  	}
-	}
-	function openNav(){
-		open = true;
-	    button.innerHTML = "-";
-	    classie.add(overlay, 'on-overlay');
-	    classie.add(wrapper, 'opened-nav');
-	}
-	function closeNav(){
-		open = false;
-		button.innerHTML = "+";
-		classie.remove(overlay, 'on-overlay');
-		classie.remove(wrapper, 'opened-nav');
-	}
-	document.addEventListener('click', closeNav);
-
-})();
-
+for (i = 0; i < 360; i += arcDegrees) {
+    var direction = _.find(directions, function(d) { return d.rotatedDegrees == i; });
+    var radians = i * (Math.PI / 180);
+    var x = centerX + Math.cos(radians) * compassRadius;
+    var y = centerY + Math.sin(radians) * compassRadius;
+        
+    var newArc = paper.path(arc.path);
+    // newArc.translate(x, y);
+    // newArc.rotate(i + 89);
+    newArc.transform('T' + x + ',' + y + 'r' + (i + 89));
+    
+    if (direction.degrees == currentlyActive) {
+        $(newArc.node).attr('id', 'activeArc');
+    }
+        
+    $(newArc.node)
+        .attr('class', 'arc')
+        .data('direction-label', direction.label)
+        .on('click', arcClicked);
+}
+});
