410501e68fd0c7fd9305be05b42b10760ad4d04d
Liam Edwards-Playne <liamzebedee@yahoo.com.au>
2015-04-19 01:58:17 +1000

    fuck it

--- public/testlogin.js
+++ public/testlogin.js
@@ -1,5 +1,5 @@
 function redirectWithToken(token) {
-	window.location = 'http://listenhere.herokuapp.com/app/?token='+token;
+	window.location = 'http://listenhere.herokuapp.com/login/?token='+token;
 }
 
 window.fbAsyncInit = function() {
