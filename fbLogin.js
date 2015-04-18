ed40ca5c222b42ba610ac6b21791df7034fead4f
Liam Edwards-Playne <liamzebedee@yahoo.com.au>
2015-04-19 00:28:45 +1000

    fuck it

--- fbLogin.js
+++ fbLogin.js
@@ -5,48 +5,45 @@ Parse.initialize("6g5rdM4QiKNQwXMCrr8tvEuHpad7mdYsjbgLRUhA", "KKRi6LU0KSyfVuKxA7
 module.exports.process = function(request, response) {
   var access_token = request.query.token;
   var user_details;
+  var musicPages;
   graph.setAccessToken(access_token);
 
   graph.get('/me', { fields: 'name,first_name,last_name,id' }, function(err, res) {
-    console.log(err);
-    console.log(res.first_name);
-    // user_details = JSON.parse(res);
-    // console.log('userd ' + user_details.prototype.toString);
-    // console.log('userd ' + user_details);
+    user_details = res;
   });
 
   
-  // graph.get('/me/music', {}, function(err, firstMusicRes) {
-  //     var musicPages = firstMusicRes.data;
-  //     var paging = firstMusicRes.paging;
+  graph.get('/me/music', {}, function(err, firstMusicRes) {
+      musicPages = firstMusicRes.data;
+      var paging = firstMusicRes.paging;
 
-  //     var i = 0;
-  //     while(paging && paging.next) {
-  //       if(i === 3) break; // test
-  //       graph.get(paging.next, function(err, res) {
-  //         paging = res.paging;
-  //         musicPages.concat(res.data);
-  //       });
-  //       i += 1;
-  //     }
+      var i = 0;
+      while(paging && paging.next) {
+        if(i === 3) break; // test
+        graph.get(paging.next, function(err, res) {
+          paging = res.paging;
+          musicPages.concat(res.data);
+        });
+        i += 1;
+      }
+    });
 
-  //     //console.log(musicPages);
-  //   });
+  // create user
+  var User = Parse.Object.extend("User");
+  var user;
+  var user_query = new Parse.Query(User);
+  user_query.equalTo("id", user_details);
+  user_query.find({success: function(found) {
+    user = found;
+  }});
 
-  // // create user
-  // var User = Parse.Object.extend("User");
-  // var newUser = new User();
-  // var user_query = new Parse.Query(User);
-  // user_query.equalTo("id", user_details);
-  // user_query.find({success: function(found) {
-  //   //console.log(JSON.stringify(found));
-  // }});
-
-  // var user_exists = false;
-  // if(!user_exists) {
-  //   newUser.set('first_name', user_details.first_name);
-  //   newUser.set('last_name', user_details.last_name);
-  //   newUser.set('id', user_details.id);
-  // }
+  var user_exists = false;
+  if(!user_exists) {
+    newUser.set('first_name', user_details.first_name);
+    newUser.set('last_name', user_details.last_name);
+    newUser.set('id', user_details.id);
+    newUser.set('music', musicPages);
+    newUser.save();
+  }
 
 };
